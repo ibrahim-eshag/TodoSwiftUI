a563868e35faac5ab7136ba957be8aaaecb5ef36
Amged <hemaisne@yahoo.com>
2022-04-06 19:03:13 +0200

    Initial Commit

---
+++ TodoSwiftUI/TodoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoSwiftUIApp.swift
+//  TodoSwiftUI
+//
+//  Created by Amged on 4/6/22.
+//
+
+import SwiftUI
+
+@main
+struct TodoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
